cb31cbba46346e86c35cb58881444dd7cfb0b7fa
Scott White <swhite@ngmoco.com>
2012-01-30 15:50:27 -0800

    adding in the blog post example

---
+++ examples/blog_example/blog_example.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"falcore"
+	"http"
+	"fmt"
+	"time"
+	"rand"
+)
+
+func main() {
+	// create pipeline
+	pipeline := falcore.NewPipeline()
+
+	// add upstream pipeline stages
+	var filter1 delayFilter
+	pipeline.Upstream.PushBack(filter1)
+	var filter2 helloFilter
+	pipeline.Upstream.PushBack(filter2)
+
+	// add request done callback stage
+	pipeline.RequestDoneCallback = reqCB
+	
+	// create server on port 8000
+	server := falcore.NewServer(8000, pipeline)
+
+	// start the server
+	// this is normally blocking forever unless you send lifecycle commands 
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("Could not start server:", err)
+	}
+}
+
+// Example filter to show timing features
+type delayFilter int
+func (f delayFilter) FilterRequest(req *falcore.Request) *http.Response {
+	status := rand.Intn(2) // random status 0 or 1
+	if status == 0 {
+		time.Sleep(rand.Int63n(100e6)) // random sleep between 0 and 100 ms
+	}
+	req.CurrentStage.Status = byte(status) // set the status to produce a unique signature
+	return nil
+}
+// Example filter that returns a Response
+type helloFilter int
+func (f helloFilter) FilterRequest(req *falcore.Request) *http.Response {
+	return falcore.SimpleResponse(req.HttpRequest, 200, nil, "hello world!\n")
+}
+
+var reqCB = falcore.NewRequestFilter(func(req *falcore.Request) *http.Response {
+	req.Trace() 	// Prints detailed stats about the request to the log
+	return nil
+})
+
