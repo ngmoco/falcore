f2c914f22c89372e971d22ac6d9634463ca1fc15
Graham Anderson <graham@andtech.eu>
2012-03-04 07:17:43 +0100

    file_filter.go: gain a few hundred requests/sec - only stat once, take filesize after stat

--- static_file/file_filter.go
+++ static_file/file_filter.go
@@ -39,14 +39,6 @@ func (f *Filter) FilterRequest(req *falcore.Request) (res *http.Response) {
 		return falcore.SimpleResponse(req.HttpRequest, 500, nil, "Server Error\n")
 	}
 
-	var fileSize int64
-	if stat, err := os.Stat(asset_path); err == nil {
-		fileSize = stat.Size()
-	} else {
-		falcore.Debug("Can't stat %v: %v", asset_path, err)
-		return falcore.SimpleResponse(req.HttpRequest, 404, nil, "File not found\n")
-	}
-
 	// Open File
 	if file, err := os.Open(asset_path); err == nil {
 		// Make sure it's an actual file
@@ -59,7 +51,7 @@ func (f *Filter) FilterRequest(req *falcore.Request) (res *http.Response) {
 				ProtoMinor:    1,
 				Body:          file,
 				Header:        make(http.Header),
-				ContentLength: fileSize,
+				ContentLength: stat.Size(),
 			}
 			if ct := mime.TypeByExtension(filepath.Ext(asset_path)); ct != "" {
 				res.Header.Set("Content-Type", ct)
