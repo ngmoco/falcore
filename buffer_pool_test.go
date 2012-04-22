5d49e835a781133ae2ebcdf1704e664faae980af
Dave Grijalva <grijalva@gmail.com>
2012-04-21 17:04:49 -0700

    test with partially read buffers as well

--- buffer_pool_test.go
+++ buffer_pool_test.go
@@ -37,5 +37,26 @@ func TestBufferPool(t *testing.T){
 		t.Errorf("Expected length %v got %v", len(text), l)
 	}
 	pool.give(bpe)
+
+	// get one
+	bpe = pool.take(bytes.NewBuffer(text))
+	// read 1 byte
+	out = make([]byte, 1)
+	bpe.buf.Read(out)
+	pool.give(bpe)
+
+	// get one
+	bpe = pool.take(bytes.NewBuffer(text))
+	// read all
+	out = make([]byte, 1024)
+	l, _ = bpe.buf.Read(out)
+	if bytes.Compare(out[0:l], text) != 0 {
+		t.Errorf("Read invalid data: %v", out)
+	}
+	if l != len(text) {
+		t.Errorf("Expected length %v got %v", len(text), l)
+	}
+	pool.give(bpe)
+
 	
 }
