ef821aeb6a031f9e8a156280adfbdb4f0f32bcf3
James Wynn <james@jameswynn.com>
2012-07-23 07:37:12 -0700

    Cleaned up the non-windows version of server functions. Compiles now.

--- server_notwindows.go
+++ server_notwindows.go
@@ -8,18 +8,18 @@ import (
 )
 
 // only valid on non-windows
-func (srv *Server) setupNonBlockingListener(srv *Server, err error, l *net.TCPListener) error {
+func (srv *Server) setupNonBlockingListener(err error, l *net.TCPListener) error {
 	if srv.listenerFile, err = l.File(); err != nil {
 		return err
 	}
 	fd := int(srv.listenerFile.Fd())
-    if e := syscall.SetNonblock(fd); e != nil {
-        return e
-    }
+	if e := syscall.SetNonblock(fd, true); e != nil {
+		return e
+	}
 	if srv.sendfile {
 		if e := syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, srv.sockOpt, 1); e != nil {
 			return e
 		}
 	}
+	return nil
 }
-
