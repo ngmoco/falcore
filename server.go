b6e88eb4b508d40ce5c569de19e0e7b4e975af54
Dave Grijalva <grijalva@gmail.com>
2012-08-16 16:21:19 -0700

    oops

--- server.go
+++ server.go
@@ -242,8 +242,8 @@ func (srv *Server) handler(c net.Conn) {
 			if res.ContentLength == 0 && len(res.TransferEncoding) == 0 && !((res.StatusCode-100 < 100) || res.StatusCode == 204 || res.StatusCode == 304) {
 				res.TransferEncoding = []string{"identity"}
 			}
-			if t.ContentLength < 0 {
-				t.TransferEncoding = []string{"chunked"}
+			if res.ContentLength < 0 {
+				res.TransferEncoding = []string{"chunked"}
 			}
 
 			// write response
